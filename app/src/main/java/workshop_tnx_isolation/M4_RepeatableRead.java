555a5678a116688fa5c803eba2f2b147e83e82c1
dorin <suleteadorin@gmail.com>
2023-08-15 18:36:25 +0200

    Nailed it

--- app/src/main/java/workshop_tnx_isolation/M4_RepeatableRead.java
+++ app/src/main/java/workshop_tnx_isolation/M4_RepeatableRead.java
@@ -16,11 +16,11 @@ public class M4_RepeatableRead {
         connector.run(conn -> {
             try (Statement st = conn.createStatement()) {
                 st.execute("DROP TABLE IF EXISTS PurchaseCart");
-                st.execute("CREATE TABLE PurchaseCart(item VARCHAR(255) PRIMARY KEY, buyer VARCHAR(255) , price INT)");
+                st.execute("CREATE TABLE PurchaseCart(item VARCHAR(255) PRIMARY KEY, buyer VARCHAR(255) , price INT, taxApplied BOOLEAN)");
 
-                st.execute("INSERT INTO PurchaseCart VALUES ('potatoes', 'Dorin', 10)");
-                st.execute("INSERT INTO PurchaseCart VALUES ('bread', 'Dorin', 5)");
-                st.execute("INSERT INTO PurchaseCart VALUES ('12v_Lead-Acid_Battery', 'Dorin', 40)");
+                st.execute("INSERT INTO PurchaseCart VALUES ('potatoes', 'Dorin', 10, false)");
+                st.execute("INSERT INTO PurchaseCart VALUES ('bread', 'Dorin', 5, false)");
+                st.execute("INSERT INTO PurchaseCart VALUES ('12v_Battery', 'Dorin', 40, false)");
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -28,7 +28,7 @@ public class M4_RepeatableRead {
         });
     }
 
-    private void payAndShip(CountDownLatch latch, String isolationLevel) {
+    private void taxPayAndShip(CountDownLatch latch, String isolationLevel) {
         connector.run(conn -> {
             try (Statement st = conn.createStatement()) {
                 st.execute("SET TRANSACTION ISOLATION LEVEL " + isolationLevel);
@@ -41,20 +41,25 @@ public class M4_RepeatableRead {
                     items.add(rs2.getString("item") + "(" + rs2.getInt("price") + ")");
                 }
 
+                // Apply a flat tax on 1 Eur per item
+                st.execute("UPDATE PurchaseCart SET price = price + 1, taxApplied=true WHERE buyer='Dorin';");
 
+                // Calculate totalPrice
                 Integer cartPrice = 0;
                 ResultSet rs = st.executeQuery("SELECT price  FROM PurchaseCart WHERE buyer='Dorin'");
                 while (rs.next()) {
                     cartPrice += rs.getInt("price");
                 }
-
-
-
-
                 rs2.close();
-                System.out.println("===================================");
+                System.out.println("==================================");
                 System.out.println("Shipping " + items + " at price " + cartPrice);
-                System.out.println("===================================");
+                System.out.println("==================================");
+
+                // Print the table
+                ResultSet rs3 = st.executeQuery("SELECT * FROM PurchaseCart;");
+                System.out.println(Util.resultSetToString("PurchaseCart", rs3, "item", "buyer", "price", "taxApplied"));
+                rs3.close();
+
 
                 st.execute("COMMIT;");
             } catch (Exception e) {
@@ -69,7 +74,7 @@ public class M4_RepeatableRead {
         connector.run(conn -> {
             try (Statement st = conn.createStatement()) {
                 st.execute("START TRANSACTION;");
-                st.execute("INSERT INTO PurchaseCart VALUES ('RaspberryPI', 'Dorin', 100)");
+                st.execute("INSERT INTO PurchaseCart VALUES ('RaspberryPI', 'Dorin', 100, false)");
                 st.execute("COMMIT;");
             } catch (Exception e) {
                 throw new RuntimeException(e);
@@ -79,6 +84,39 @@ public class M4_RepeatableRead {
         });
     }
 
+
+    /**
+     * REPEATABLE READ guarantees that : "the selects within a transaction will see the same row values even if other transactions changed said values".
+     * But it claims nothing about new rows or deleted rows. It makes sense really, how can a transaction acquire a lock on a row that does not yet exist?
+     **/
+
+    /**
+     * What happens here:
+     * 1) `taxPayAndShip` transaction does the initial select to determine which items are supposed to be shipped for "Dorin"
+     * 2) `addToCart` concurrently INSERTS a new row for Dorin.
+     * Now the total number of rows involved has increased, but since this row was created after the `taxPayAndShip` transaction stated it has no
+     * lock or any other concurrency protection.
+     * 3) `taxPayAndShip` adds the tax, and sums up the total cost (now including the newly added row) which was not accounted for in the first select.
+     * We end up charging the user for an item they won't ever receive.
+     **/
+
+    /**
+     * This is called "Phantom Read" anomaly. New rows can pop in and out of existence between statements of a transactions.
+     */
+
+    /**
+     * Setting the isolation level to SERIALIZABLE solves this problem.
+     * *
+     * The naive way to achieve this isolation level is to put a big lock on the entire table "nothing changes while I run my transaction".
+     * Which would yield correct results but will be highly inefficient.
+     * *
+     * Real systems use a concept called "range locks" instead of a global mutex.
+     * While a serializable transaction is active, the DBMS will maintain a set of "currently active where clauses"
+     * For every new insertion the system will check if the new record matching any of these active clauses.
+     * If yes, the row insertion is throttled, it must wait for the active SERIALIZABLE transaction to end.
+     * If not it can go ahead as there is no risk the new row will pop up in any running serializable transactions.
+     */
+
     public static void main(String[] args) throws InterruptedException {
         M4_RepeatableRead sc = new M4_RepeatableRead();
         sc.createSchema();
@@ -87,8 +125,20 @@ public class M4_RepeatableRead {
         CountDownLatch latch = new CountDownLatch(2);
 
         exec.execute(() -> sc.addToCart(latch));
-        exec.execute(() -> sc.payAndShip(latch, "REPEATABLE READ"));
+        exec.execute(() -> sc.taxPayAndShip(latch, "REPEATABLE READ"));
 
+        // SERIALIZABLE is the strongest isolation level but also the most lock intensive and bad for performance.
+        // Serilizability is defined as such : "equivalence of outcome to a serial/sequential schedule".
+        // Said more street casually :
+        // a) if I run `taxPayAndShip` and `addToCart` sequentially that yields consistent state.
+        // b) if I run `addToCart` and `taxPayAndShip` sequentially that also yields consistent state.
+        // c) if I run both `taxPayAndShip` and `addToCart` concurrently and I get the same outcome as A or B that's good,
+        // there is no concurrency error.
+
+        // If we run the same code in serializable isolation level we get B ordering of mutations which is a valid outcome.
+        // Playing with the sleep delays can force the example to give us outcome A, which would also be valid.
+
+        // exec.execute(() -> sc.taxPayAndShip(latch, "SERIALIZABLE"));
 
         latch.await();
         exec.shutdown();
